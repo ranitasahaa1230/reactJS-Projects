809a42a0d6bb7a4a1541b896fa9b57a5b85ebf43
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-07 01:01:48 +0530

    Update InputData.jsx

--- keep-clone/src/InputData.jsx
+++ keep-clone/src/InputData.jsx
@@ -4,6 +4,7 @@ export default function InputData() {
   const [todo,setTodo]=useState([]);
   const [input,setInput]=useState("");
   const [text,setText]=useState("");
+  const [colour,setColour]=useState("aqua")
 
   const clickHandler=(e)=>{
     e.preventDefault();
@@ -13,18 +14,22 @@ export default function InputData() {
 
   return (
     <div>
-        <form className="form-text">
+        <form className="form-text" style={{backgroundColor:colour}}>
           <input type="text" placeholder="Title" value={input} onChange={(e)=>setInput(e.target.value)}/>
           <br/>
           <textarea placeholder="Take a note..." cols="80" rows="5" value={text} onChange={(e)=>setText(e.target.value)}/>
           <br/>
-          <button type="submit" onClick={clickHandler}>Submit</button>
+          <button onClick={clickHandler}>Pin It</button>
+          <button type='submit' onClick={clickHandler}>Submit</button>
+          <button onClick={()=>setColour("yellow")}>Yellow</button>
+          <button onClick={()=>setColour("green")}>Green</button>
+          <button onClick={()=>setColour("blue")}>Blue</button>
         </form>
         <div className='todo-flex'>
         {todo.map((todos)=>{
           return(
             <div key={todos.id} className="todo-border">
-              <p>{todos.input}</p>
+              <h2>{todos.input}</h2>
               <p>{todos.text}</p>
             </div>
           )
