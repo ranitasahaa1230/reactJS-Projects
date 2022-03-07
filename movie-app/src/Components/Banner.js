9ab5c5b123b9702d347046cb141cf45487fb707d
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-07 16:49:15 +0530

    update banners

--- movie-app/src/Components/Banner.js
+++ movie-app/src/Components/Banner.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { movies } from './getMovies'
 
 export default function Banner() {
-    const movie=movies.results[3];
-
+    const movie=movies.results[0];
+    // const movie='';
   return (
     <>
     {movie==="" ? 
@@ -12,7 +12,7 @@ export default function Banner() {
 </div>
 :
     <div className="card">
-  <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top" alt={movie.title}/>
+  <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img" alt={movie.title}/>
     <h5 className="card-title">{movie.original_title}</h5>
     <p className="card-text">{movie.overview}</p>
 </div>
