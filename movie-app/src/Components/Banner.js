a99ef573f1b2bf6f62151d71dab568d6aa3f3733
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-07 14:14:00 +0530

    update

--- movie-app/src/Components/Banner.js
+++ movie-app/src/Components/Banner.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { movies } from './getMovies'
 
-
 export default function Banner() {
-    const movie=movies.results[0];
+    const movie=movies.results[3];
+
   return (
     <>
-    {movie==="" ? <div class="spinner-border text-primary" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>:
-    <div className="card" style="width: 18rem;">
+    {movie==="" ? 
+    <div className="spinner-border text-primary" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+:
+    <div className="card">
   <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top" alt={movie.title}/>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">{movie.original_title}</h5>
+    <p className="card-text">{movie.overview}</p>
 </div>
 }
 </>
