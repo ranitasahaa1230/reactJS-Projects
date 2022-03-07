2bfe4314f04f161dea56ccca8fd5f25c2321b0f6
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-07 17:13:53 +0530

    update

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
+import Movies from './Components/Movies';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
+      <Movies/>
     </div>
   );
 }
---
+++ movie-app/src/Components/Movies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Movies() {
+  return (
+    <div>
+        <h2>Trending Movies</h2>
+    </div>
+  )
+}
