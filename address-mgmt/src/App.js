ff0165af338afcd3c3c6bad8d51ce4aa58bd6614
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-13 23:55:07 +0530

    update

--- address-mgmt/src/App.js
+++ address-mgmt/src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+// import './App.css';
+// import React, { useEffect, useState } from 'react'
+// import {axios} from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+// function App() {
+//   const [address,setAddress]=useState('')
 
-export default App;
+//   const URL="http://localhost:5000/address"
+
+//   const getAddress=async()=>{
+//     const response=await axios.get(URL);
+//     console.log(response)
+//   }
+
+//   useEffect(()=>{
+//     getAddress()
+//   },[])
+
+//   return (
+//     <div className="App">
+//       <h1>Manage your addresses</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const numArray = [ [3, 10, 11, 12], [1, 2,], [3, 4],5,6];
+const flat=numArray=>numArray.reduce((acc,cval)=>[...acc,...cval],[])
+console.log(flat(numArray))
