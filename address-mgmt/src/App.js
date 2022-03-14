e7cd2c3d01e0b4eb83bde2146216a73aa42a4c06
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-14 20:06:18 +0530

    update

--- address-mgmt/src/App.js
+++ address-mgmt/src/App.js
@@ -1,30 +1,49 @@
-// import './App.css';
-// import React, { useEffect, useState } from 'react'
-// import {axios} from "axios";
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 
-// function App() {
-//   const [address,setAddress]=useState('')
+function App() {
+  const [address,setAddress]=useState([]);
+  const [namee,setName]=useState('')
 
-//   const URL="http://localhost:5000/address"
+  const DataAPI = "https://62188b391a1ba20cbaa3c9ce.mockapi.io/api/users";
 
-//   const getAddress=async()=>{
-//     const response=await axios.get(URL);
-//     console.log(response)
-//   }
+  const getAddress=async()=>{
+    const {data}=await axios.get(DataAPI);
+    console.log(data)
+  }
 
-//   useEffect(()=>{
-//     getAddress()
-//   },[])
+  useEffect(()=>{
+    getAddress()
+  },[])
 
-//   return (
-//     <div className="App">
-//       <h1>Manage your addresses</h1>
-//     </div>
-//   );
-// }
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+  }
+  return (
+    <div className="App">
+      <h1>Manage your addresses</h1>
+      <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='Name' value={namee} onChange={(e)=>setName(e.target.value)}/>
+          {/* <input type="number" placeholder='Mobile No'value={mobile} />
+          <input type="number" placeholder='Pincode' value={pincode}/>
+          <input type="text" placeholder='City' value={city}/>
+          <input type="text" placeholder='Address' value={address}/>
+          <input type="text" placeholder='State' value={state}/> */}
+        <button type='submit'>Submit</button>
+        <button type='submit'>Reset</button>
+      </form>
 
-// export default App;
+      <div>
+          {address.map((item)=>(
+              <div key={item.id}>
+                    <div>{item.name}</div>
+              </div>
+          ))}
+      </div>
+    </div>
+  );
+}
 
-const numArray = [ [3, 10, 11, 12], [1, 2,], [3, 4],5,6];
-const flat=numArray=>numArray.reduce((acc,cval)=>[...acc,...cval],[])
-console.log(flat(numArray))
+export default App;
+
