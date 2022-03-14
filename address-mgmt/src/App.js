80c4b43f16e86fd75250e78e38a85a874b1f28ec
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-15 00:00:03 +0530

    update

--- address-mgmt/src/App.js
+++ address-mgmt/src/App.js
@@ -8,24 +8,30 @@ import FormController from "./components/FormController";
 function App() {
   const [address, setAddress] = useState([]);
   const [error, setError] = useState(false);
-  const [showForm, setShowForm] = useState(false);
+  const [displayForm, setDisplayFormToggle] = useState(false);
 
   const DataAPI = "https://62188b391a1ba20cbaa3c9ce.mockapi.io/api/users";
+
+  const defaultAddress = {
+    name: "",
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    phone: ""
+  };
 
   const getAddress = async () => {
     try {
       const { data } = await axios.get(DataAPI);
       setAddress(data);
     } catch (err) {
-      setError(true);
+      setError(true)
       console.log("error", err.description);
     }
   };
 
 
-  const toggleInputForm=()=>{
-    setShowForm(()=>!showForm)
-  }
 
   useEffect(() => {
     getAddress();
@@ -33,21 +39,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Name' value={namee} onChange={(e)=>setName(e.target.value)}/>
-          {/* <input type="number" placeholder='Mobile No'value={mobile} />
-          <input type="number" placeholder='Pincode' value={pincode}/>
-          <input type="text" placeholder='City' value={city}/>
-          <input type="text" placeholder='Address' value={address}/>
-          <input type="text" placeholder='State' value={state}/> */}
-      {/* <button type='submit'>Submit</button>
-        <button type='submit'>Reset</button>
-      </form> */}
-
       {!error ? <Header /> : <ErrorHeader/>}
-      {!showForm && <FormController toggle={toggleInputForm}/>}
-      {/* {address.length===0 ? (<h3>No Address to display</h3>)
-      : ()} */}
+      
     </div>
   );
 }
--- address-mgmt/src/components/InputForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function InputForm() {
-  return (
-    <div>InputForm</div>
-  )
-}
