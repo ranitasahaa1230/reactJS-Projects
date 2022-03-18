dcf756149154359122f10523ad62fd3e73d3f9cb
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-18 09:28:07 +0530

    update

--- search-filter-sort/src/App.js
+++ search-filter-sort/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const columns = [
-    { title: "Athlete", field: "athlete" },
-    { title: "Age", field: "age" },
-    { title: "Country", field: "country" },
-    { title: "Year", field: "year" },
-    { title: "Date", field: 'date' },
-    { title: "Sport", field: 'sport' },
-    { title: "Gold", field: 'gold' },
-    { title: "Silver", field: 'silver' },
-    { title: "Bronze", field: 'bronze' },
-    { title: "Total", field: 'total' },
-  ]
-  
+ 
   return (
     <div className="App">
-      
+      <h1>hhhh</h1>
     </div>
   );
 }
