b550d9f1435cf5c049dcd242e494659583d207e6
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-09 22:29:33 +0530

    sass files

--- video-library/src/App.js
+++ video-library/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
+import { Sidebar } from './components/Sidebar/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      
+    <Navbar/>
+    <div>
+      <Sidebar/>
+    </div>
     </div>
   );
 }
---
+++ video-library/src/components/HomeScreen/HomeSreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HomeSreen = () => {
+  return (
+    <div>HomeSreen</div>
+  )
+}
