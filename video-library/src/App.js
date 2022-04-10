a511d4a31baf875dec485d85edde7a57ef5f3aa6
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-10 13:30:51 +0530

    base import

--- video-library/src/App.js
+++ video-library/src/App.js
@@ -1,16 +1,22 @@
-import './App.css';
-import { Navbar } from './components/Navbar/Navbar';
-import { Sidebar } from './components/Sidebar/Sidebar';
-
+import "./App.css";
+import { useState } from "react";
+import { Header, Home, Login, NotFound } from "./components";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [sidebars, toggleSidebar] = useState(false);
+
+  const handleToggleSidebar = () => toggleSidebar((prev) => !prev);
   return (
-    <div className="App">
-    <Navbar/>
-    <div>
-      <Sidebar/>
-    </div>
-    </div>
+    <>
+    <Header handleToggleSidebar={handleToggleSidebar}/>
+    <Routes>
+    <Route path="/" element={<Home sidebars={sidebars} handleToggleSidebar={handleToggleSidebar} />}/>
+      <Route path="/auth" element={<Login/>}/>
+      <Route path="*" element={<NotFound/>}/>
+
+    </Routes>
+    </>
   );
 }
 
