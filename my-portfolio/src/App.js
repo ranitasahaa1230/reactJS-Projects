e2286e4cfe7c1f23eb9f4626dcc24b4a08d5be6e
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-10 13:34:41 +0530

    completed tailwind

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
+import { Skills } from './components/Skills';
+import { Work } from './components/Work';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+     <Navbar/>
+     <Home/>
+     <About/>
+     <Skills/>
+     <Work/>
+     <Contact/>
     </div>
   );
 }
