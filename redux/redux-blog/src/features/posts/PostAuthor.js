50416649d033fc229f6acb0a9ee76fa46f27dab2
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-23 23:09:26 +0530

    update posts

---
+++ redux/redux-blog/src/features/posts/PostAuthor.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PostAuthor = () => {
+  return (
+    <div>PostAuthor</div>
+  )
+}
---
+++ redux/redux-blog/src/features/posts/TimeAgo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TimeAgo = () => {
+  return (
+    <div>TimeAgo</div>
+  )
+}
