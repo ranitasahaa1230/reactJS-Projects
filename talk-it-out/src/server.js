2d44e574c791a36c8eb09d97a72f324a39008f67
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-24 23:26:39 +0530

    update

--- talk-it-out/src/server.js
+++ talk-it-out/src/server.js
@@ -79,26 +79,26 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/posts/dislike/:postId", dislikePostHandler.bind(this));
 
       //post comments routes (public)
-      this.get("/comments/:postId", getPostCommentsHandler.bind(this));
+      // this.get("/comments/:postId", getPostCommentsHandler.bind(this));
 
       //post comments routes (private)
-      this.post("/comments/add/:postId", addPostCommentHandler.bind(this));
-      this.post(
-        "/comments/edit/:postId/:commentId",
-        editPostCommentHandler.bind(this)
-      );
-      this.post(
-        "/comments/delete/:postId/:commentId",
-        deletePostCommentHandler.bind(this)
-      );
-      this.post(
-        "/comments/upvote/:postId/:commentId",
-        upvotePostCommentHandler.bind(this)
-      );
-      this.post(
-        "/comments/downvote/:postId/:commentId",
-        downvotePostCommentHandler.bind(this)
-      );
+      // this.post("/comments/add/:postId", addPostCommentHandler.bind(this));
+      // this.post(
+      //   "/comments/edit/:postId/:commentId",
+      //   editPostCommentHandler.bind(this)
+      // );
+      // this.post(
+      //   "/comments/delete/:postId/:commentId",
+      //   deletePostCommentHandler.bind(this)
+      // );
+      // this.post(
+      //   "/comments/upvote/:postId/:commentId",
+      //   upvotePostCommentHandler.bind(this)
+      // );
+      // this.post(
+      //   "/comments/downvote/:postId/:commentId",
+      //   downvotePostCommentHandler.bind(this)
+      // );
       // user routes (public)
       this.get("/users", getAllUsersHandler.bind(this));
       this.get("/users/:userId", getUserHandler.bind(this));
