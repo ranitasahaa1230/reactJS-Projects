7c00e99a1330efd647e7dd1488781056f4d33e24
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-25 00:25:52 +0530

    update images

--- talk-it-out/src/App.js
+++ talk-it-out/src/App.js
@@ -2,17 +2,22 @@ import "./styles/App.scss";
 import Navigation from "./components/Navigation";
 import Cards from "./components/Cards";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./componentss";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <main>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigation />}/>
+        {/* <main>
         <div className="container">
           <Cards />
           <Sidebar />
         </div>
-      </main>
+      </main> */}
+      </Routes>
     </div>
   );
 }
--- talk-it-out/src/components/App.js
+++ talk-it-out/src/components/App.js
@@ -1,20 +1,20 @@
-import "../styles/App.scss";
-import Navigation from "./Navigation";
-import Cards from "./Cards";
-import Sidebar from "./Sidebar";
+// import "../styles/App.scss";
+// import Navigation from "./Navigation";
+// import Cards from "./Cards";
+// import Sidebar from "./Sidebar";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <main>
-        <div className="container">
-          <Cards />
-          <Sidebar />
-        </div>
-      </main>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navigation />
+//       <main>
+//         <div className="container">
+//           <Cards />
+//           <Sidebar />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
